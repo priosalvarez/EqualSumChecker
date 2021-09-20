0b25444d952a69f8b5caf9c07300a9c56022ceae
Pedro A. Rios <odautu@gmail.com>
2021-09-20 17:39:27 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum.hasEqualSum(1,4,5));
+    }
+}
---
+++ src/hasEqualSum.java
@@ -0,0 +1,10 @@
+public class hasEqualSum {
+    public static boolean hasEqualSum(int number1, int number2, int number3){
+
+        if (number1 + number2 == number3){
+            return true;
+        }
+
+        return false;
+    }
+}
